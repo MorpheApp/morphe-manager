b9eb34574e0a1e6782f61dcb3448bf7431401e27
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-12-09 05:14:15 +0200

    fix: Use morphe namespace for plugin manifest

--- api/src/main/kotlin/app/revanced/manager/plugin/downloader/Constants.kt
+++ api/src/main/kotlin/app/revanced/manager/plugin/downloader/Constants.kt
@@ -1,7 +1,7 @@
 package app.revanced.manager.plugin.downloader
 
 /**
- * The permission ID of the special plugin host permission. Only ReVanced Manager will have this permission.
+ * The permission ID of the special plugin host permission. Only Morphe Manager will have this permission.
  * Plugin UI activities and internal services can be protected using this permission.
  */
-const val PLUGIN_HOST_PERMISSION = "app.universal.revanced.manager.permission.PLUGIN_HOST"
+const val PLUGIN_HOST_PERMISSION = "app.morphe.manager.permission.PLUGIN_HOST"
