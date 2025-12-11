c9d82d87b9d40bf02093b157b0e1842709b6e7c0
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2025-12-11 09:24:44 +0200

    Comment out dead code

--- app/src/main/java/app/revanced/manager/patcher/runtime/ProcessRuntime.kt
+++ app/src/main/java/app/revanced/manager/patcher/runtime/ProcessRuntime.kt
@@ -134,12 +134,15 @@ class ProcessRuntime(private val context: Context) : Runtime(context) {
         // Get the location of our own Apk.
         val managerBaseApk = pm.getPackageInfo(context.packageName)!!.applicationInfo!!.sourceDir
 
+        if (false) { // Morphe begin. Memory limit is handled automatically
         val requestedLimit = prefs.patcherProcessMemoryLimit.get()
         val sanitizedLimit = MemoryLimitConfig.clampLimitMb(context, requestedLimit)
         if (sanitizedLimit != requestedLimit) {
             Log.w(tag, "Requested process memory limit ${requestedLimit}MB exceeded device capabilities; clamped to ${sanitizedLimit}MB")
         }
         val limit = "${sanitizedLimit}M"
+        } // Morphe end
+
         val propOverride = resolvePropOverride(context)?.absolutePath
             ?: throw Exception("Couldn't find prop override library")
 
