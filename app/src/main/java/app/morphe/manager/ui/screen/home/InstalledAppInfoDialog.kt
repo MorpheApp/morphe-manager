03efb9adc795cc8623ce29ea74798a6cc724ee3a
MarcaDian <152095496+MarcaDian@users.noreply.github.com>
2026-05-15 23:25:50 +0300

    fix build

--- app/src/main/java/app/morphe/manager/ui/screen/home/InstalledAppInfoDialog.kt
+++ app/src/main/java/app/morphe/manager/ui/screen/home/InstalledAppInfoDialog.kt
@@ -22,6 +22,7 @@ import androidx.compose.material.icons.automirrored.outlined.List
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
